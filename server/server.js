8a87f55a47fd2ffdd089af7d2fcc6a6188532243
16Yongjin <yongjin0802@gmail.com>
2017-05-19 21:13:00 +0900

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,6 +47,23 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('¯\_(ツ)_/¯');
+    }
+
+    Todo.findByIdAndRemove(id).then((doc) => {
+        if(!doc) {
+            return res.status(404).send('Todo not found');
+        }
+        res.send(doc);
+    },(e) => {
+        res.status(400).send(e);
+    })
 })
 
 app.listen(port, () => {
